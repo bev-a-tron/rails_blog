1f947bd312acc6de1e3208abb8dfda194798961a
Beverly Lau <beverly.a.lau@gmail.com>
2015-12-29 18:56:47 -0800

    Add ActiveRecord

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  get '/' => 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
