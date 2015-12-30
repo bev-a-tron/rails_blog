04440a21b7637fb621a7a03df05758fa64dcb443
Beverly Lau <beverly.a.lau@gmail.com>
2015-12-30 10:24:33 -0800

    Add dropdown menu in Admin site

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Article do
-
+  menu parent: "Blog"
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
