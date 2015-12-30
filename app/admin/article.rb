ae614c66b6ee6e9f50160c7ba4592f8ecf868d62
Beverly Lau <beverly.a.lau@gmail.com>
2015-12-30 10:40:15 -0800

    Permit_params are needed for ActiveAdmin to create new objects

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Article do
   menu parent: "Blog"
+  permit_params :title, :text
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
