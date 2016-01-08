3df5b595714465d6d6123b2e898d8a8b29a27501
Beverly Lau <beverly.a.lau@gmail.com>
2016-01-08 15:40:40 -0800

    Test for mocking and stubbing with and without return values

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -13,9 +13,48 @@ RSpec.describe ArticlesController do
 
   describe '#beverlysMethod' do
     let(:controller) { ArticlesController.new }
-    it 'should return 100' do
-      answer = controller.beverlys_method
-      expect(answer).to be(100)
+
+    context 'without mocking' do
+      it 'should return value from method' do
+        answer = controller.beverlys_method
+        expect(answer).to eq('aaa')
+      end
+    end
+
+    context 'with mocking' do
+      context 'without return' do
+        it 'should return nil' do
+          expect(controller).to receive(:beverlys_method)
+          answer = controller.beverlys_method
+          expect(answer).to eq(nil)
+        end
+      end
+
+      context 'with return' do
+        it 'should return the return value' do
+          expect(controller).to receive(:beverlys_method).and_return('value')
+          answer = controller.beverlys_method
+          expect(answer).to eq('value')
+        end
+      end
+    end
+
+    context 'with stubbing' do
+      context 'with return' do
+        it 'should return the return value' do
+          allow(controller).to receive(:helper_method).and_return('cake')
+          t = controller.helper_method(2345678)
+          expect(t).to eq('cake')
+        end
+      end
+
+      context 'without return' do
+        it 'should return nil' do
+          allow(controller).to receive(:helper_method)
+          t = controller.helper_method(2345678)
+          expect(t).to eq(nil)
+        end
+      end
     end
   end
 
